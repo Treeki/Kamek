cdcd13ca79dbd24955777d260c6083c2e57ee82d
Trevor <49212064+kiwi515@users.noreply.github.com>
2023-02-22 17:03:14 -0500

    Merge from upstream (#1)

--- examples/build-1.bat
+++ examples/build-1.bat
@@ -1,5 +1,5 @@
 ..\cw\mwcceppc -I- -i ../k_stdlib -Cpp_exceptions off -enum int -Os -use_lmw_stmw on -fp hard -rostr -sdata 0 -sdata2 0 -c -o 1-nsmbw-osreport.o 1-nsmbw-osreport.cpp
-..\Kamek\bin\Debug\Kamek 1-nsmbw-osreport.o -static=0x80341E68 -externals=externals-nsmbw-eu-v1.txt -output-riiv=1-loader.xml -output-gecko=1-loader.txt -output-code=1-loader.bin
-..\Kamek\bin\Debug\Kamek 1-nsmbw-osreport.o -dynamic -externals=externals-nsmbw-eu-v1.txt -versions=versions-nsmbw.txt -output-kamek=1-dynamic.$KV$.bin
+..\Kamek\bin\Debug\net6.0\Kamek 1-nsmbw-osreport.o -static=0x80341E68 -externals=externals-nsmbw-eu-v1.txt -output-riiv=1-loader.xml -output-gecko=1-loader.txt -output-code=1-loader.bin
+..\Kamek\bin\Debug\net6.0\Kamek 1-nsmbw-osreport.o -dynamic -externals=externals-nsmbw-eu-v1.txt -versions=versions-nsmbw.txt -output-kamek=1-dynamic.$KV$.bin
 
 
